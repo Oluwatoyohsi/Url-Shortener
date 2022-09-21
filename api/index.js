5ac5399f9117c7f764d9bbc630922daca251deb5
Amire Esther <amireesther17@gmail.com>
2022-09-21 12:10:09 +0100

    setup for deployment

--- api/index.js
+++ api/index.js
@@ -1,23 +1,16 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 5000;
-const fs = require('fs/promises');
-const shortURL = require('./shortURL');
+const shortURL = require('../shortURL');
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+ 
+let data = [];
 
 app.post('/', shortURL, async (req, res) => {
  
-  let data;
-  try {
-    data = await fs.readFile(path.resolve('data.json'), 'utf-8');
-    data = JSON.parse(data);
-  } catch (error) {
-    data = [];
-  }
   res.shorturl = `${req.protocol}://${req.get('host')}/${res.shortURL}`;
   let newUrl = {
     url: req.body.url,
@@ -25,7 +18,6 @@ app.post('/', shortURL, async (req, res) => {
     slug: res.shortURL
   };
   data.push(newUrl);
-  await fs.writeFile(path.resolve('data.json'), JSON.stringify(data));
   res.status(200).json(newUrl)
  // res.redirect('/');
   console.log(req.body);
@@ -38,14 +30,7 @@ app.post('/', shortURL, async (req, res) => {
 });
 
 app.get('/:slug', async (req, res) => {
-  let smallUrl;
-  try{
-    smallUrl = await fs.readFile("data.json", "utf-8");
-    smallUrl = JSON.parse(smallUrl)
-  } catch(error) {
-    smallUrl = [];
-  }
-  let getURL = smallUrl.find((link)=>{
+  let getURL = data.find((link)=>{
     return link?.slug === req.params.slug
   });
   if(getURL){
@@ -61,7 +46,4 @@ app.get('/', (req, res) => {
   res.sendFile(path.resolve('index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`server is running on http://localhost:${port}`);
-});
 module.exports = app;
